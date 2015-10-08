95a85ba0e385dc9767491a883328437f44e8e82c
Christophe Riccio <christopheri@unity3d.com>
2015-10-08 03:15:31 +0200

    Added convert code and tests

--- test/core/core_generate_mipmaps.cpp
+++ test/core/core_generate_mipmaps.cpp
@@ -32,6 +32,87 @@
 #include <gli/copy.hpp>
 #include <gli/generate_mipmaps.hpp>
 
+#include <glm/gtc/epsilon.hpp>
+
+namespace detail
+{
+	template <typename retType, typename T, gli::precision P, template <typename, gli::precision> class vecType, bool normalize = false, bool srgb = false>
+	struct convert
+	{
+		static vecType<retType, P> fetch(gli::texture2D const & Texture, gli::texture2D::dim_type const & TexelCoord, gli::texture2D::size_type Level)
+		{
+			return vecType<retType, P>(Texture.load<vecType<T, P> >(TexelCoord, Level));
+		}
+
+		static void write(gli::texture2D & Texture, gli::texture2D::dim_type const & TexelCoord, gli::texture2D::size_type Level, vecType<retType, P> const & Texel)
+		{
+			Texture.store<vecType<T, P> >(TexelCoord, Level, vecType<T, P>(Texel));
+		}
+	};
+
+	template <typename retType, typename T, gli::precision P, template <typename, gli::precision> class vecType>
+	struct convert<retType, T, P, vecType, true, false>
+	{
+		static vecType<retType, P> fetch(gli::texture2D const & Texture, gli::texture2D::dim_type const & TexelCoord, gli::texture2D::size_type Level)
+		{
+			return gli::compNormalize<retType>(Texture.load<vecType<T, P> >(TexelCoord, Level));
+		}
+
+		static void write(gli::texture2D & Texture, gli::texture2D::dim_type const & TexelCoord, gli::texture2D::size_type Level, vecType<retType, P> const & Texel)
+		{
+			Texture.store<vecType<T, P> >(TexelCoord, Level, glm::compScale<T>(Texel));
+		}
+	};
+
+	template <typename retType, typename T, gli::precision P, template <typename, gli::precision> class vecType>
+	struct convert<retType, T, P, vecType, true, true>
+	{
+		static vecType<retType, P> fetch(gli::texture2D const & Texture, gli::texture2D::dim_type const & TexelCoord, gli::texture2D::size_type Level)
+		{
+			return gli::convertSRGBToLinear(gli::compNormalize<retType>(Texture.load<vecType<T, P> >(TexelCoord, Level)));
+		}
+
+		static void write(gli::texture2D & Texture, gli::texture2D::dim_type const & TexelCoord, gli::texture2D::size_type Level, vecType<retType, P> const & Texel)
+		{
+			Texture.store<vecType<T, P> >(TexelCoord, Level, gli::compScale<T>(gli::convertLinearToSRGB(Texel)));
+		}
+	};
+}//namespace detail
+
+namespace texel
+{
+	int test()
+	{
+		int Error = 0;
+
+		{
+			gli::texture2D Texture(gli::FORMAT_RGBA8_UINT, gli::texture2D::dim_type(1), 1);
+			detail::convert<glm::u8, glm::u8, gli::defaultp, gli::tvec4, false, false>::write(Texture, gli::texture2D::dim_type(0), 0, gli::u8vec4(255, 127, 0, 255));
+			glm::u8vec4 Texel = detail::convert<glm::u8, glm::u8, gli::defaultp, gli::tvec4, false, false>::fetch(Texture, gli::texture2D::dim_type(0), 0);
+
+			Error += Texel == gli::u8vec4(255, 127, 0, 255) ? 0 : 1;
+		}
+
+		{
+			gli::texture2D Texture(gli::FORMAT_RGBA8_UNORM, gli::texture2D::dim_type(1), 1);
+			detail::convert<float, glm::u8, gli::defaultp, gli::tvec4, true, false>::write(Texture, gli::texture2D::dim_type(0), 0, gli::vec4(1.0f, 0.5f, 0.0f, 1.0f));
+			glm::vec4 Texel = detail::convert<float, glm::u8, gli::defaultp, gli::tvec4, true, false>::fetch(Texture, gli::texture2D::dim_type(0), 0);
+
+			Error += glm::all(glm::epsilonEqual(Texel, gli::vec4(1.0f, 0.5f, 0.0f, 1.0f), 0.01f)) ? 0 : 1;
+		}
+
+		{
+			gli::texture2D Texture(gli::FORMAT_RGBA8_SRGB, gli::texture2D::dim_type(1), 1);
+			detail::convert<float, glm::u8, gli::defaultp, gli::tvec4, true, true>::write(Texture, gli::texture2D::dim_type(0), 0, gli::vec4(1.0f, 0.5f, 0.0f, 1.0f));
+			glm::vec4 Texel = detail::convert<float, glm::u8, gli::defaultp, gli::tvec4, true, true>::fetch(Texture, gli::texture2D::dim_type(0), 0);
+
+			Error += glm::all(glm::epsilonEqual(Texel, gli::vec4(1.0f, 0.5f, 0.0f, 1.0f), 0.01f)) ? 0 : 1;
+		}
+
+		return Error;
+	}
+}//namespace texel
+
 namespace mipmaps
 {
 	int test()
@@ -73,6 +154,7 @@ int main()
 	int Error = 0;
 
 	Error += mipmaps::test();
+	Error += texel::test();
 
 	return Error;
 }
