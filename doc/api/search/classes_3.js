1571b1f7dbf1ea4d9f05ac7a6ac4236b03b06f34
Christophe Riccio <christopheri@unity3d.com>
2016-02-16 03:13:25 +0100

    Fixed man pages

--- doc/api/search/classes_3.js
+++ doc/api/search/classes_3.js
@@ -6,6 +6,6 @@ var searchData=
   ['sampler2d',['sampler2d',['../a00017.html',1,'gli']]],
   ['sampler2d_5farray',['sampler2d_array',['../a00018.html',1,'gli']]],
   ['sampler3d',['sampler3d',['../a00019.html',1,'gli']]],
-  ['samplercube',['samplerCube',['../a00020.html',1,'gli']]],
-  ['samplercubearray',['samplerCubeArray',['../a00021.html',1,'gli']]]
+  ['sampler_5fcube',['sampler_cube',['../a00020.html',1,'gli']]],
+  ['sampler_5fcube_5farray',['sampler_cube_array',['../a00021.html',1,'gli']]]
 ];
