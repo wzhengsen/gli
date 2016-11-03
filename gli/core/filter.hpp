638063ae74e4a5c6981b8fdffc1c1c03778ada97
Christophe Riccio <christopheri@unity3d.com>
2016-11-03 21:42:30 +0100

    clean up

--- gli/core/filter.hpp
+++ gli/core/filter.hpp
@@ -5,17 +5,6 @@
 
 namespace gli
 {
-/*
-namespace detail
-{
-	enum dimension
-	{
-		DIMENSION_1D,
-		DIMENSION_2D,
-		DIMENSION_3D
-	};
-}//namespace detail
-*/
 	/// Texture filtring modes
 	enum filter
 	{
